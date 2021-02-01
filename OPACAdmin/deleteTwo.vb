7ab6ff14721d93ce337685f4599d4ee2a1b0480f
Joenelle Canonizado <61829759+whtvrimElle@users.noreply.github.com>
2021-02-01 15:11:12 +0800

    Add project files.

---
+++ OPACAdmin/deleteTwo.vb
@@ -0,0 +1,3 @@
+﻿Public Class deleteTwo
+
+End Class
---
+++ OPACAdmin/returnOne.vb
@@ -0,0 +1,12 @@
+﻿Public Class returnOne
+    Private Sub confirmButton_Click(sender As Object, e As EventArgs) Handles confirmButton.Click
+        returnForm.statusUpdate(returnForm.callNumberShare, returnForm.userIdShare, "Returned", returnForm.costShare)
+        customMessageBoxTwo.pagePanel(returnTwo)
+        returnTwo.successText.Visible = False
+        BunifuTransition1.ShowSync(returnTwo.successText, True)
+    End Sub
+
+    Private Sub BunifuFlatButton2_Click(sender As Object, e As EventArgs) Handles BunifuFlatButton2.Click
+        customMessageBoxTwo.Close()
+    End Sub
+End Class
